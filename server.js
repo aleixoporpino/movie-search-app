1a4dca9c1098f9a40057c57c9482ef739c01d680
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2023-02-06 18:14:01 -0500

    Fix refresh path on express and remove yarn install step on deploy

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '/build/') });
+});
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), () => {

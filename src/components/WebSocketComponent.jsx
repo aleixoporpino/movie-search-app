1876955a85a0f456a169d516da8553906e1f3647
Aleixo Porpino <aleixoporpino@gmail.com>
2024-10-11 19:49:39 -0400

    Add websocket config to count how many users are on page

---
+++ src/components/WebSocketComponent.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Badge } from '@mui/material';
+import Person from '@mui/icons-material/Person';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+
+const WebSocketComponent = ({ page }) => {
+  const [, setSocket] = useState(null);
+  const [clientCount, setClientCount] = useState(0);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:9090/ws');
+    setSocket(ws);
+
+    ws.onopen = () => {
+      ws.send(JSON.stringify({ page }));
+    };
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (data.clientCount !== undefined) {
+        setClientCount(data.clientCount);
+      }
+    };
+
+    return () => ws.close();
+  }, [page]);
+
+  return (
+    <div>
+      {clientCount !== 0 ? (
+        <>
+          <Box
+            sx={{
+              position: 'fixed',
+              bottom: 20,
+              left: 0,
+              right: 25,
+              textAlign: 'right',
+              transition: 'opacity 0.3s',
+              '&:hover': {
+                opacity: 1,
+              },
+              opacity: 0.6,
+            }}
+          >
+            <Badge
+              badgeContent={clientCount}
+              color='success'
+              sx={{
+                transition: 'opacity 0.3s',
+                '&:hover': {
+                  opacity: 1,
+                },
+                opacity: 0.6,
+              }}
+            >
+              <Tooltip title={`Users on this page ${clientCount}`}>
+                <Person color='action' />
+              </Tooltip>
+            </Badge>
+          </Box>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default WebSocketComponent;
+
+WebSocketComponent.propTypes = {
+  page: PropTypes.string.isRequired,
+};
